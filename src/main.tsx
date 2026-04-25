73b0e83221cacfb46799b06560309b9817744bcb
K1rrrkvz <126238623+k1rrrkvz@users.noreply.github.com>
2026-04-25 18:13:03 +0000

    Generated by Spark: Fix all reported errors.

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ErrorBoundary } from "react-error-boundary";
-import "@github/spark/spark"
 
 import App from './App.tsx'
 import { ErrorFallback } from './ErrorFallback.tsx'
@@ -8,9 +8,12 @@ import { ErrorFallback } from './ErrorFallback.tsx'
 import "./main.css"
 import "./styles/theme.css"
 import "./index.css"
+import "@github/spark/spark"
 
 createRoot(document.getElementById('root')!).render(
-  <ErrorBoundary FallbackComponent={ErrorFallback}>
-    <App />
-   </ErrorBoundary>
+  <StrictMode>
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
+      <App />
+    </ErrorBoundary>
+  </StrictMode>
 )
